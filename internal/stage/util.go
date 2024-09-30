3682a36fde3d59bb8d9a900ba32da02ed9f13acb
Boming Zhang <bomingzh@sjtu.edu.cn>
2024-09-30 00:48:27 -0400

    feat: decode conf error msg

--- internal/stage/util.go
+++ internal/stage/util.go
@@ -1,12 +1,16 @@
 package stage
 
-import "github.com/mitchellh/mapstructure"
+import (
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+)
 
 func DecodeConf[T any](confAny any) (*T, error) {
 	var conf T
 	err := mapstructure.Decode(confAny, &conf)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to decode conf: %w", err)
 	}
 	return &conf, nil
 }
