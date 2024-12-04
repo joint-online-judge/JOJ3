5abda79a544572ae2903f5d4cb0c8fd3003bea2b
Boming Zhang <bomingzh@sjtu.edu.cn>
2024-12-04 06:35:39 -0500

    feat(parser/file): force quit on non empty conf

--- internal/parser/file/parser.go
+++ internal/parser/file/parser.go
@@ -7,7 +7,8 @@ import (
 )
 
 type Conf struct {
-	Name string
+	Name                string
+	ForceQuitOnNonEmpty bool
 }
 
 type File struct{}
@@ -20,12 +21,16 @@ func (*File) Run(results []stage.ExecutorResult, confAny any) (
 		return nil, true, err
 	}
 	var res []stage.ParserResult
+	forceQuit := false
 	for _, result := range results {
 		content := result.Files[conf.Name]
+		if conf.ForceQuitOnNonEmpty && content != "" {
+			forceQuit = true
+		}
 		if !strings.HasSuffix(content, "\n") {
 			content += "\n"
 		}
 		res = append(res, stage.ParserResult{Score: 0, Comment: content})
 	}
-	return res, false, nil
+	return res, forceQuit, nil
 }
