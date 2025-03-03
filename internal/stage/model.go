187b5f70572aaa632f36daec194a5d2eb854b2f9
Boming Zhang <bomingzh@sjtu.edu.cn>
2025-03-02 20:59:29 -0500

    chore(stage): detail log field name

--- internal/stage/model.go
+++ internal/stage/model.go
@@ -222,16 +222,16 @@ func (s NonNullSlice[T]) MarshalJSON() ([]byte, error) {
 type StageResult struct {
 	Name      string                     `json:"name"`
 	Results   NonNullSlice[ParserResult] `json:"results"`
-	ForceQuit bool                       `json:"force_quit"`
+	ForceQuit bool                       `json:"force_quit"` // underscore as it will dump to file
 }
 
 type CaseDetail struct {
-	Index          int
-	ExecutorResult ExecutorResult
-	ParserScores   map[string]int
+	Index          int            `json:"index"`
+	ExecutorResult ExecutorResult `json:"executorResult"`
+	ParserScores   map[string]int `json:"parserScores"`
 }
 
 type StageDetail struct {
-	Name        string
-	CaseDetails []CaseDetail
+	Name        string       `json:"name"`
+	CaseDetails []CaseDetail `json:"caseDetails"`
 }
