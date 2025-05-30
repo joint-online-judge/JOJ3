015ff49021d8ea00518409b29d996a19befd26af
BoYanZh <boyanzh233@gmail.com>
2025-05-31 06:20:34 +0800

    feat(healthcheck): succeed msg (#88)
    Reviewed-on: https://focs.ji.sjtu.edu.cn/git/JOJ/JOJ3/pulls/88
    Co-authored-by: BoYanZh <boyanzh233@gmail.com>
    Co-committed-by: BoYanZh <boyanzh233@gmail.com>

--- pkg/healthcheck/all.go
+++ pkg/healthcheck/all.go
@@ -19,31 +19,43 @@ func All(
 	if err != nil {
 		res.Msg += fmt.Sprintf("### Repo Size Check Failed:\n%s\n", err.Error())
 		res.Failed = true
+	} else {
+		res.Msg += "### Repo Size Check Passed\n"
 	}
 	err = ForbiddenCheck(rootDir)
 	if err != nil {
 		res.Msg += fmt.Sprintf("### Forbidden File Check Failed:\n%s\n", err.Error())
 		res.Failed = true
+	} else {
+		res.Msg += "### Forbidden File Check Passed\n"
 	}
 	err = MetaCheck(rootDir, metaFile)
 	if err != nil {
 		res.Msg += fmt.Sprintf("### Meta File Check Failed:\n%s\n", err.Error())
 		res.Failed = true
+	} else {
+		res.Msg += "### Meta File Check Passed\n"
 	}
 	err = NonASCIIFiles(rootDir)
 	if err != nil {
 		res.Msg += fmt.Sprintf("### Non-ASCII Characters File Check Failed:\n%s\n", err.Error())
 		res.Failed = true
+	} else {
+		res.Msg += "### Non-ASCII Characters File Check Passed\n"
 	}
 	err = NonASCIIMsg(rootDir)
 	if err != nil {
 		res.Msg += fmt.Sprintf("### Non-ASCII Characters Commit Message Check Failed:\n%s\n", err.Error())
 		res.Failed = true
+	} else {
+		res.Msg += "### Non-ASCII Characters Commit Message Check Passed\n"
 	}
 	err = VerifyFiles(rootDir, checkFileNameList, checkFileSumList)
 	if err != nil {
 		res.Msg += fmt.Sprintf("### Repo File Check Failed:\n%s\n", err.Error())
 		res.Failed = true
+	} else {
+		res.Msg += "### Repo File Check Passed\n"
 	}
 	return res
 }
